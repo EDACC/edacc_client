0d983f7155bea593b2ed2a596ec2418910fd5fba
Daniel D <daniel.diepold@gmail.com>
2012-03-19 17:02:09 +0100

    added python version of SAT verifier

---
+++ verifiers/PySAT.py
@@ -0,0 +1,48 @@
+import sys
+
+def iabs(x):
+    return x if x >= 0 else -x
+
+def exit_verifier(result_code, exit_code):
+    print result_code
+    sys.exit(exit_code)
+
+if __name__ == '__main__':
+    instance_path = sys.argv[1]
+    solver_output_path = sys.argv[2]
+    
+    variables = {}
+    SAT_answer = False
+    with open(solver_output_path) as solver_output:
+        for line in solver_output:
+            tokens = line.split()
+            if len(tokens) < 2: continue
+            prefix = tokens[1]
+            if prefix == "s":
+                answer = tokens[2]
+                if answer == "UNKNOWN":
+                    print "Solver reported unknown."
+                    exit_verifier(0, 0)
+                elif answer == "SATISFIABLE":
+                    print "Solver reported satisfiable. Checking solution."
+                    SAT_answer = True
+                elif answer == "UNSATISFIABLE":
+                    print "Solver reported unsatisfiable. I guess it must be right!"
+                    exit_verifier(10, 0)
+            elif prefix == "v":
+                for var in map(int, tokens[2:]): variables[iabs(var)] = var
+
+    if SAT_answer:
+        with open(instance_path) as instance:
+            for line in instance:
+                if line[0] in ('c', 'p'): continue
+                clause = map(int, line.split())
+                if not clause or not any(variables[iabs(var)] == var for var in clause):
+                    print "Clause", clause, "not satisfied"
+                    print "Wrong solution!"
+                    exit_verifier(0, 0)
+        print "Solution verified."
+        exit_verifier(11, 0)
+    else:
+        print "Didn't find anything interesting in the output"
+        exit_verifier(0, 0)
